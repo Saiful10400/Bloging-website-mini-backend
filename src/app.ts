aa469da098beb94a24aa07cdbe2c14e347b5e970
saiful10400 <saifulislam10400i5@gmail.com>
2025-08-29 13:12:34 +0600

    cors updated.

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ import globalErrorHandler from "./MiddleWare/globalErrorHandler";
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: [ "http://localhost:3000"], credentials: true }));
+app.use(cors({ origin: [ "http://localhost:3000","https://blog10400.vercel.app"], credentials: true }));
 app.use("/api", routes);
 
 app.use(globalErrorHandler);
